29a7d6bf992973a780e411ee4b30d9e70f9d296a
Eiji <64912886+Eiji127@users.noreply.github.com>
2023-09-18 14:45:41 +0900

    [Add]: add day1 challenge project.

---
+++ Day1/Day1/Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  Day1App.swift
+//  Day1
+//
+//  Created by 白数叡司 on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
