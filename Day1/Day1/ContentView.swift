d0ef2a2a1660fc4e5173e21705a5fe6dda5c3c82
Eiji <64912886+Eiji127@users.noreply.github.com>
2023-09-18 14:51:17 +0900

    [Change]: indicate "Hello, visionOS!!" on window.

--- Day1/Day1/ContentView.swift
+++ Day1/Day1/ContentView.swift
@@ -11,20 +11,8 @@ import RealityKitContent
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView {
-            List {
-                Text("Item")
-            }
-            .navigationTitle("Sidebar")
-        } detail: {
-            VStack {
-                Model3D(named: "Scene", bundle: realityKitContentBundle)
-                    .padding(.bottom, 50)
-
-                Text("Hello, world!")
-            }
-            .navigationTitle("Content")
-            .padding()
+        NavigationStack {
+            Text("Hello, visionOS!!")
         }
     }
 }
